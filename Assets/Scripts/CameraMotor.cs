57bb5cd61f31c79acddafdc8b135411d7c852ae5
Jeofferson Dela Pena <jeoffersond@gmail.com>
2020-08-17 19:32:35 +0800

    Segment 17 done

--- Assets/Scripts/CameraMotor.cs
+++ Assets/Scripts/CameraMotor.cs
@@ -10,6 +10,7 @@ public class CameraMotor : MonoBehaviour
 
     public Transform lookAt;
     public Vector3 offset;
+    public Vector3 rotation;
 
 
     private void LateUpdate()
@@ -19,13 +20,14 @@ public class CameraMotor : MonoBehaviour
 
         Vector3 desiredPosition = lookAt.position + offset;
 
-        Vector3 desiredPositionWithLerp = Vector3.Lerp(transform.position, desiredPosition, (Time.deltaTime * 2f));
+        Vector3 desiredPositionWithLerp = Vector3.Lerp(transform.position, desiredPosition, Time.deltaTime * 2f);
 
         // To remove the lerp when jumping or changing lanes
         desiredPositionWithLerp.x = desiredPosition.x;
         desiredPositionWithLerp.y = desiredPosition.y;
+        transform.position = desiredPositionWithLerp;
 
-        transform.position = desiredPositionWithLerp; 
+        transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.Euler(rotation), .1f);
 
     }
 
