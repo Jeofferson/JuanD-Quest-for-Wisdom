2f03738a2c4e5f8e60ba7d4a2d169d5f349ee0cc
Jeofferson Dela Pena <jeoffersond@gmail.com>
2020-08-22 17:48:09 +0800

    Pause menu part 1 done

---
+++ Assets/Scripts/PauseManager.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+
+public class PauseManager : MonoBehaviour
+{
+
+
+    private GameManager gameManager;
+
+    public Animator animatorPauseManager;
+
+
+    private void Awake()
+    {
+
+        gameManager = FindObjectOfType<GameManager>();
+
+    }
+
+
+    public void OnButtonPause()
+    {
+
+        Time.timeScale = 0f;
+        animatorPauseManager.SetTrigger("Show");
+
+    }
+
+
+    public void OnButtonHome()
+    {
+
+        Time.timeScale = 1f;
+        gameManager.LoadSceneGame(false);
+
+    }
+
+
+    public void OnButtonResume()
+    {
+
+        Time.timeScale = 1f;
+        animatorPauseManager.SetTrigger("Hide");
+
+    }
+
+
+}
