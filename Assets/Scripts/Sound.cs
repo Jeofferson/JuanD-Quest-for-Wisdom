73534f25dea16143b7034d1c7b4547150558741e
Jeofferson Dela Pena <jeoffersond@gmail.com>
2020-09-19 22:43:07 +0800

    Started writing the script for audio manager

---
+++ Assets/Scripts/Sound.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine.Audio;
+using UnityEngine;
+
+
+[System.Serializable]
+public class Sound
+{
+
+
+    public string name;
+
+    public AudioClip audioClip;
+
+    [HideInInspector]
+    public AudioSource audioSource;
+
+    [Range(0f, 1f)]
+    public float volume;
+    [Range(.1f, 3f)]
+    public float pitch;
+
+
+}
